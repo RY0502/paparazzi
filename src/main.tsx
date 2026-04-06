31107f721cf3ac193b14e51c7bdd5de1f4ce0c71
RY0502 <ravi_yadav0502@yahoo.in>
2026-04-06 16:51:21 +0530

    Add service worker update check

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.ready.then(reg => {
+    reg.update();
+  });
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
